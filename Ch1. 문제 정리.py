0894d5027fafd07323486ca429c34a3959641026
백민정 <110668121+yangheeb@users.noreply.github.com>
2022-10-12 13:44:57 +0900

    Add files via upload

---
+++ Ch1. 문제 정리.py
@@ -0,0 +1,124 @@
+#p.49 중간점검 // 반복문 어떻게 설정해서 코드 만들 지 생각해보기 //
+
+#01
+import turtle
+t=turtle.Turtle()
+
+t.shape("turtle")
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+
+turtle.mainloop()
+turtle.bye()
+
+#02
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+
+turtle.mainloop()
+turtle.bye()
+
+#03
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+t.left(60)
+t.forward(100)
+
+turtle.mainloop()
+turtle.bye()
+
+
+#p.55 programming 예제
+
+#01
+print("환영합니다.")
+print("파이썬의 세계로 오신 것을 환영합니다.")
+print("파이썬은 강력합니다.")
+
+#02
+print("+"+"="*16+"+")
+print("l"+" "*5+"홍길동"+" "*5+"ㅣ")
+print("+"+"="*16+"+")
+
+#03
+print(1+2+3)
+
+#04
+print("1+2+3을 계산하면",1+2+3,"입니다.")
+
+#05 
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.right(90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.lt(90)
+t.fd(100)
+turtle.mainloop()
+turtle.bye()
+
+#06 //체크해보기
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+t.forward(300)
+t.lt(90)
+t.fd(200)
+turtle.mainloop()
+turtle.bye()
+
+#07
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+t.width(10)   #다시해보기
+t.forward(100)
+t.lt(90)
+t.fd(120)
+turtle.mainloop()
+turtle.bye()
+
+#08
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+t.color("blue")
+t.forward(100)
+turtle.mainloop()
+turtle.bye()
+
+#09
+import turtle
+t=turtle.Turtle()
+t.shape("square")
+t.forward(100)
+turtle.mainloop()
+turtle.bye()
