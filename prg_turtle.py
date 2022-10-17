2eb6c354a5538b5360ed790db064e3976f55fa4c
백민정 <110668121+yangheeb@users.noreply.github.com>
2022-10-17 15:03:43 +0900

    week2

---
+++ prg_turtle.py
@@ -0,0 +1,13 @@
+#2215641 백민정 터틀 그래픽 프로그램
+import turtle as t #turtle 모듈을 가져와서 별명을 t라 지정함
+t.shape('turtle')
+t.color('blue')
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(150)
+t.right(90)
+t.forward(150)
+
+t.done()
---
+++ prg_var.py
@@ -0,0 +1,13 @@
+#2215641 백민정 (변수관련 프로그램)
+a1=100 #변수이름이 a1기억장소에 100이라는 정수를 기억(대입)함
+print(a1)
+_b=57.8
+print(_b,type(_b))#type()함수는 자료형을
+c_123="경영정보학과"
+print(c_123,type(c_123))
+
+import1=True
+print(import1,type(import1))
+
+a1=9999
+print(a1,type(a1))
