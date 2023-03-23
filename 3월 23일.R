45d876865db18805b172bb834aacafa0a448fe62
백민정 <110668121+yangheeb@users.noreply.github.com>
2023-03-23 10:07:25 +0900

    Add files via upload

---
+++ 3월 23일.R
@@ -0,0 +1,9 @@
+sqrt(100)
+d<-c(1,7,4,2,3)  #벡터d에 5개의 값을 저장
+sort(d)  #벡터 d의 값들을 오름차순으로 정렬하여 출력
+sort(d,decreasing=TRUE)  #벡터 d의 값들을 내림차순으로 정렬하여 출력
+sort(x=d,decreasing = FALSE)
+sort(d,FALSE)
+
+str<-paste('good','morning',sep='/')
+str
